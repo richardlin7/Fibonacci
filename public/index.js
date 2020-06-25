d2ca790b84714428e4947cae57c67d212129c87d
Richard Lin <rlin7@student.gsu.edu>
2020-06-24 20:10:52 -0500

    Add Button that uses api to obtain random number for fibonacci function

--- public/index.js
+++ public/index.js
@@ -1,7 +1,21 @@
+//const { response } = require("express");
+
 function onClick() {
   // recieve input from text box
   const n = document.getElementById("input").value;
   // use input into fibonacci(n)
   // output result of function to log
   document.getElementById("output").textContent = fibonacci(n);
+}
+
+function onClick1() {
+  fetch("http://162.243.173.140:4000")
+    .then((response) => {
+      return random();
+    })
+    .then((users) => {
+      console.log(users);
+    });
+
+  document.getElementById("output").textContent = 0;
 }
