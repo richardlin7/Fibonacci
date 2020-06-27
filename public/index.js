8b2b5f2d7772185b6237a215602e222e4286aab6
Richard Lin <rlin7@student.gsu.edu>
2020-06-27 16:43:06 -0500

    modified changes

--- public/index.js
+++ public/index.js
@@ -2,10 +2,7 @@
 var url = "http://162.243.173.140:8080";
 
 function onClick() {
-  // recieve input from text box
   const n = document.getElementById("input").value;
-  // use input into fibonacci(n)
-  // output result of function to log
   document.getElementById("output").textContent = fibonacci(n);
 }
 
