d68a68ed8e7ba88a7ccc0a2634c85293ca77df5d
Richard Lin <rlin7@student.gsu.edu>
2020-07-07 16:34:26 -0500

    added functional upper/lower bounds

--- public/index.js
+++ public/index.js
@@ -1,14 +1,29 @@
-//const { response } = require("express");
-var url = "http://162.243.173.140:8000/";
-// var url = "http://localhost:8000/";
+// const url = "http://162.243.173.140:8000";
+const url = "http://localhost:8000";
 
 function onClick() {
   const n = document.getElementById("input").value;
   document.getElementById("output").textContent = fibonacci(n);
 }
 
+// working function but commented out for reference :D
 function onClick1() {
-  fetch(url)
+  var x = document.getElementById("max").value;
+  var y = document.getElementById("min").value;
+
+  const data = { high: x, low: y };
+
+  fetch(url, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(data),
+  })
+    // .then((response) => response.json())
+    // .then((body) => console.log(body))
     .then(function (response) {
       return response.text();
     })
@@ -22,3 +37,25 @@ function onClick1() {
       console.error("There is a issue with your fetch operation: ", error);
     });
 }
+
+// function onClick1() {
+//   //function in progress, integrating upper and lower bound first.
+
+//   fetch(url)
+//     .then(function (response) {
+//       return response.text();
+//     })
+//     .then(function (text) {
+//       document.getElementById("random").textContent = text;
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       console.error("There is a issue with your fetch operation: ", error);
+//     });
+
+//   var num = document.getElementById("random").value;
+//   console.log(num);
+//   document.getElementById("output").textContent = fibonacci(num);
+// }
